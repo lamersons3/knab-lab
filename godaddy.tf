00e2df610e57ca6512b8a9ff16c8c49b43666368
lamersons2 <to@mikez.online>
2024-11-29 00:07:21 +0100

    fix1

--- godaddy.tf
+++ godaddy.tf
@@ -1,10 +1,10 @@
-resource "godaddy_domain_record" "knab_lab_godaddy_cname" {
-  domain = "mikez.online"
+# resource "godaddy_domain_record" "knab_lab_godaddy_cname" {
+#   domain = "mikez.online"
 
-  record {
-    name = "knab-lab"
-    type = "CNAME"
-    data = aws_elb.knab_lab_lb.dns_name
-    ttl  = 1800
-  }
-}
+#   record {
+#     name = "knab-lab"
+#     type = "CNAME"
+#     data = aws_elb.knab_lab_lb.dns_name
+#     ttl  = 1800
+#   }
+# }
